a48482c8e95e80d3d696cb3ed4f758f42d47d62f
Thanish C <146630791+heytanix@users.noreply.github.com>
2025-02-24 10:59:59 +0530

    Add files via upload

---
+++ common/constants.ts
@@ -0,0 +1,13 @@
+export enum EventType {
+    SPORTS = 1000,
+    VOLEYBALL = 1001,
+    FOOTBALL = 1002,
+    CRICKET = 1100,
+    CULTURAL = 2000,
+}
+
+export enum Role {
+    ADMIN = 'admin',
+    USER = 'user',
+    GUEST = 'guest',
+}
